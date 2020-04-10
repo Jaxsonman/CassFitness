85db3f3ebd0a899262b6a09e616626e20d639326
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2020-04-10 12:20:28 -0600

    master

--- CassSite/settings.py
+++ CassSite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-PROD = True
+PROD = False
 if PROD:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 else:
